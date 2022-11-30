3935c82ee8ae964f6df7c19e02dccb948bb8771f
MB-Snow <119429521+MB-Snow@users.noreply.github.com>
2022-11-30 15:00:25 +0000

    Update streamline_app.py
    Added 'Kale, Spinach & Rocket Smoothie' and 'Hard-boiled Free-Range Egg' to menu

--- streamline_app.py
+++ streamline_app.py
@@ -6,3 +6,7 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blue Berry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-boiled Free-Range Egg')
