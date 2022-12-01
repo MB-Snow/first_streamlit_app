d8f51d56eaafdda79c00c0b8ef025f98544f5b1e
MB-Snow <119429521+MB-Snow@users.noreply.github.com>
2022-12-01 16:49:09 +0000

    Update streamline_app.py
    Updated section to display Fruityvice API response

--- streamline_app.py
+++ streamline_app.py
@@ -1,7 +1,6 @@
 
 import streamlit
 import pandas
-import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -24,8 +23,9 @@ fruits_to_show = my_fruit_list.loc[fuits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# New section to display Fruityvice  API response
+# New section to display Fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
