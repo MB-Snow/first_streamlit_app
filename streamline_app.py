e98d19a29cc8b89faeaa262c57cfa2aff32c53e1
MB-Snow <119429521+MB-Snow@users.noreply.github.com>
2022-12-05 09:36:48 +0000

    Update streamline_app.py
    added snowflake connector

--- streamline_app.py
+++ streamline_app.py
@@ -36,3 +36,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displays Fruity Vice data as a table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
